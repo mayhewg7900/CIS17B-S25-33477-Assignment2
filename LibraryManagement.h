94df27a5c82cc8c69b1aa54ddfe789b94eaeb31c
Matthew <matthewgaramone@gmail.com>
2025-03-12 14:27:00 -0700

    Updates to header file, and a main.cpp file added with some tests

--- LibraryManagement.h
+++ LibraryManagement.h
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <cctype>
+#include <memory>
+#include <vector>
 
 using namespace std;
 
@@ -16,6 +18,7 @@ class Book {
         void setAvailability(bool a) {
             availability = a;
         }
+        string getISBN() { return ISBN; }
 };
 
 class User {
@@ -29,16 +32,40 @@ class User {
         int getID() { return userID; }
 };
 
+class Student : public User {
+public:
+    Student(string& n, int& i) : User(n, i) {}
+};
+
+class Faculty : public User {
+public:
+    Faculty(string& n, int& i) : User(n, i) {}
+};
+
 class Library {
-    
+    // Vector of pointers to dynamically allocated memory for the User class instances
+    vector<unique_ptr<User>> users;
+    // Vector of pointers to dynamically allocated memory for the Book class instances
+    vector<unique_ptr<Book>> books;
+
+    public:
+        // Adding derived student class to the vector with unique pointers to the User class instances
+        void registerStudent(string n, int i) {
+            users.push_back(make_unique<Student>(n, i));
+          }
+        // Adding derived Faculty class to the vector with unique pointers to the User class instances
+        void registerFaculty(string n, int i) {
+            users.push_back(make_unique<Student>(n, i));
+        }
+        // Adding instance of the Book class to the vector with unique pointers to these instances
+        void addBook(string n, string a, string i) {
+            // Looping through each pointer to each instance of Book classes in the books vector
+            for (unique_ptr<Book> &book : books) {
+                if (book->getISBN() == i) {
+                    cout << "\nError, book could not be added at the moment, only adding one copy of a book is currently supported.\n";
+                }
+            }
+            books.push_back(make_unique<Book>(n, a, i, true));
+        }
 };
 
-class Student : User {
-    public:
-        Student(string n, int i): User(n, i) {}
-};
-
-class Faculty : User {
-    public:
-        Faculty(string n, int i): User(n, i) {}
-};
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include "LibraryManagement.cpp"
+
+int main() {
+    Library library;
+    library.addBook("Test", "test2", "123");
+    library.addBook("Test", "test2", "123");
+}
